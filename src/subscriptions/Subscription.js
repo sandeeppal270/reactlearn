ecf71e7234b750150ecf0dcd781c5dd6374a7710
sandeeppal270 <64672009+sandeeppal270@users.noreply.github.com>
2022-11-03 19:03:25 +0530

    first commit

--- src/subscriptions/Subscription.js
+++ src/subscriptions/Subscription.js
@@ -3,13 +3,14 @@ import SubscriptionDate from './SubscriptionDate'
 import Container from '../templates/Container'
 const Subscription = (props) =>{
     // this is js function four using month day year in string format
-   
-    return(
+//    document.getElementById('changeTitleButton').addEventListener()
+    const onClickHandler= () => {}
+return(
         <Container className="subscription">
         <SubscriptionDate date={props.date}/>
         <h2 className='subscription_title'>{props.title}</h2>
         <div className='subscription_price'>{props.amount}</div>
-        <button type="button" id="changeTitleButton">Change Title</button>
+        <button type="button" id="changeTitleButton" onClick={onClickHandler()}>Change Title</button>
         </Container>
     )
 }
