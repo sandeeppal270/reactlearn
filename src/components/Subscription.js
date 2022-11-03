10dfe6cac1fbe71ecd5286179eefa05dfc978d92
sandeeppal270 <64672009+sandeeppal270@users.noreply.github.com>
2022-11-03 16:46:35 +0530

    first commit

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,14 +1,15 @@
 import './Subscription.css'
 import SubscriptionDate from './SubscriptionDate'
+import Container from './Container'
 function Subscription(props){
     // this is js function four using month day year in string format
    
     return(
-        <div className="subscription">
+        <Container className="subscription">
         <SubscriptionDate date={props.date}/>
         <h2 className='subscription_title'>{props.title}</h2>
         <div className='subscription_price'>{props.amount}</div>
-        </div>
+        </Container>
     )
 }
 export default Subscription
