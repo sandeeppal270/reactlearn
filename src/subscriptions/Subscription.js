7a2e3cd19fcecfb2103050a2f3a22a1cc29bf5a7
sandeeppal270 <64672009+sandeeppal270@users.noreply.github.com>
2022-11-04 12:05:23 +0530

    first commit

--- src/subscriptions/Subscription.js
+++ src/subscriptions/Subscription.js
@@ -1,14 +1,23 @@
-import './Subscription.css'
-import SubscriptionDate from './SubscriptionDate'
-import Container from '../templates/Container'
-const Subscription = (props) =>{
+import './Subscription.css';
+import SubscriptionDate from './SubscriptionDate';
+import Container from '../templates/Container';
+import React,{useState} from 'react';
+// import React from 'react';
+// import { useState } from 'react';
+const Subscription = (props) => {
+    const [title,setTitle]=useState(props.title)
     // this is js function four using month day year in string format
-//    document.getElementById('changeTitleButton').addEventListener()
-    const onClickHandler= () => {}
+//document.getElementById('changeTitleButton').addEventListener()
+    const onClickHandler= () => {
+    //title="Changed Title";
+    setTitle("Changed Title")
+    console.log("On button clicked",title)
+    }
+
 return(
         <Container className="subscription">
         <SubscriptionDate date={props.date}/>
-        <h2 className='subscription_title'>{props.title}</h2>
+        <h2 className='subscription_title'>{title}</h2>
         <div className='subscription_price'>{props.amount}</div>
         <button type="button" id="changeTitleButton" onClick={onClickHandler()}>Change Title</button>
         </Container>
